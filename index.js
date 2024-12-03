d6b321f73d5c68f0ed12d79ef3b61a865007c03c
Chakawala2004 <armanchakawalajob@gmail.com>
2024-12-03 14:32:55 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function EmployeeInfo(name,age,salary){
+    console.log(name,age,salary);
+}
+EmployeeInfo("arman", 20,5000);
+
+let emploueeName="Arman";
+let employeeAge= 20;
+let employeeSalary="5000";
+
+
+
